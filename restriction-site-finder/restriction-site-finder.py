278e9d3252e46a39086b969092a97a41afbfced4
Manuel Gehl <127943149+ManuelGehl@users.noreply.github.com>
2024-01-06 16:36:37 +0100

    First working version

--- restriction-site-finder/restriction-site-finder.py
+++ restriction-site-finder/restriction-site-finder.py
@@ -23,6 +23,7 @@ class RestrictionSiteFinder:
         self.rev_complement = []
         self.window_size = self.WINDOW_SIZE
         self.motifs = {}
+        self.results = {}
         
         
     def read_sequence(self):
@@ -38,19 +39,7 @@ class RestrictionSiteFinder:
         # Put sequences in string
         self.dna_sequence = "".join(seq_dict.values())
     
-    def reverse_complement(self):
-        # Reverse DNA sequence
-        self.rev_complement = self.dna_sequence[::-1]
-        # Define mapping dictionary
-        mapping_dict = {"A": "T",
-                        "T": "A",
-                        "G": "C",
-                        "C": "G"}
-        # Map characters in sequence
-        complement_seq = [mapping_dict[char] for char in self.rev_complement]
-        self.rev_complement = "".join(complement_seq)
-    
-    def create_substrings(self, sequence):
+    def create_substrings(self):
         """
         Generates substrings from DNA sequence and reverse complement sequence.
         Window size ranges from 4 to 12.
@@ -58,52 +47,65 @@ class RestrictionSiteFinder:
         
         """
         # Determine lenght of sequence
-        sequence_length = len(sequence)
+        sequence_length = len(self.dna_sequence)
         # Generate empty dictionary to store window size and nested dictionary
         window_dict = {}
         # Loop trough different window sizes
         for window_size in range(4, 13):
             # Determine how many steps for given window size
-            step_size = sequence_length - window_size
+            step_size = sequence_length - (window_size - 1)
             # Generate empty dictionary to store positions and substrings
             pos_substrings = {}
             # Iterate over whole sequence and store positions and substrings
             for step in range(step_size):
-                substring = sequence[step:step + window_size]
+                substring = self.dna_sequence[step:step + window_size]
                 pos_substrings[step + 1] = substring
             # Store values in dictionary
             window_dict[window_size] = pos_substrings
-        
-        return window_dict
-        
+        self.substrings = window_dict
     
-    def find_palindrom(self, dna_substrings, rev_substrings):
+    def reverse_complement(self, sequence):
+        # Reverse DNA sequence
+        rev_seq = sequence[::-1]
+        # Define mapping dictionary
+        mapping_dict = {"A": "T",
+                        "T": "A",
+                        "G": "C",
+                        "C": "G"}
+        # Map characters in sequence
+        complement_seq = [mapping_dict[char] for char in rev_seq]
+        return "".join(complement_seq)
+    
+    def find_palindrom(self):
         """
-        Checks if DNA substring is equal to its reverse complement at same position.
+        Checks if DNA substring is equal to its reverse complement.
         """
-        for window_size in range(4, 13):
-            for pos in dna_substrings[window_size]:
-                if dna_substrings[window_size][pos] == rev_substrings[window_size][pos]:
-                    print(f"Pos1 {pos}")
-                    print(f"DNA {dna_substrings[pos]}")
-                    print(f"Rev {rev_substrings[pos]}")
+        for window_size in self.substrings:
+            for pos in self.substrings[window_size]:
+                # Take one subsequence
+                for_seq = self.substrings[window_size][pos]
+                # Reverse complement that sequence
+                rev_seq = self.reverse_complement(sequence=for_seq)
+                # Check if forward sequence matches reverse complement
+                if for_seq == rev_seq:
+                    self.results[pos] = window_size
                     
-
-        
     def write_result(self):
         """
         Write result to the output file.
         """
+        # First sort results based on positions
+        self.results = dict(sorted(self.results.items()))
         with open(self.output_path, "w") as file:
-            file.write(f"{self.longest_motif}")      
-""""
-restriction_finder = RestrictionSiteFinder()
-restriction_finder.read_sequence()
-restriction_finder.reverse_complement()
-dna_substrings = restriction_finder.create_substrings(sequence=restriction_finder.dna_sequence)
-rev_substrings = restriction_finder.create_substrings(sequence=restriction_finder.rev_complement)
-restriction_finder.find_palindrom(dna_substrings=dna_substrings, rev_substrings=rev_substrings)
+            for pos, size in self.results.items():
+                file.write(f"{pos} {size}\n")      
+
+def main():
+    restriction_finder = RestrictionSiteFinder()
+    restriction_finder.read_sequence()
+    restriction_finder.create_substrings()
+    restriction_finder.find_palindrom()
+    restriction_finder.write_result()
 
 if __name__ == "__main__":
     main()
-"""
