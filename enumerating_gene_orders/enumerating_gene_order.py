9a87444265be3fed76c6d7c513b56b90447cc23d
Manuel Gehl <127943149+ManuelGehl@users.noreply.github.com>
2024-02-20 15:51:39 +0100

    Exchanged factorial function

--- enumerating_gene_orders/enumerating_gene_order.py
+++ enumerating_gene_orders/enumerating_gene_order.py
@@ -11,12 +11,16 @@ class PermutationList:
     
     def input(self) -> int:
         gene_number =  int(input("Please enter a number of genes for permutation between 0 and 7: "))
-        assert gene_number <= 7, "Please enter a number less or equal to 7"
-        assert gene_number > 0, "Please enter a number greater or equal to 1"
+        assert (gene_number <= 7), "Please enter a number less or equal to 7"
+        assert (gene_number > 0), "Please enter a number greater or equal to 1"
         self.gene_number = gene_number
     
     def total_permutations(self) -> None:
-        self.total_perm = math.factorial(self.gene_number)
+        # Calculate the factorial
+        factorial = 1
+        for factor in range(2, self.gene_number+1):
+            factorial *= factor
+        self.total_perm = factorial
     
     def generate_stock(self) -> List[int]:
         self.stock = list(range(1, self.gene_number+1)) * self.total_perm
