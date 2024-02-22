5e13df01316dd2891921dcb2b9ad8709264c43d2
Manuel Gehl <127943149+ManuelGehl@users.noreply.github.com>
2024-02-22 17:26:17 +0100

    Final polished version

--- enumerating_gene_orders/enumerating_gene_order.py
+++ enumerating_gene_orders/enumerating_gene_order.py
@@ -2,31 +2,55 @@ from typing import List
 import random
 
 class PermutationList:
+    """
+    A class to represent a permutation list.
+
+    Attributes:
+    - gene_number: int, the number of genes for permutation.
+    - total_perm: int, the total number of permutations.
+    - stock: List[int], the stock of available numbers for permutations.
+    - permutations: List[List[int]], the generated permutations.
+    """
     
     def __init__(self):
+        """
+        Initializes a PermutationList object with default values.
+        """
         self.gene_number = None
         self.total_perm = None
-        self.freq = None
         self.stock = None
         self.permutations = None
     
-    def input(self) -> int:
+    def input(self) -> None:
+        """
+        Takes user input for the number of genes for permutation.
+        Ensures the input is between 1 and 7.
+        """
         gene_number =  int(input("Please enter a number of genes for permutation between 0 and 7: "))
         assert (gene_number <= 7), "Please enter a number less or equal to 7"
         assert (gene_number > 0), "Please enter a number greater or equal to 1"
         self.gene_number = gene_number
     
     def total_permutations(self) -> None:
+        """
+        Calculates the total number of permutations based on the gene number.
+        """
         # Calculate the factorial
         factorial = 1
         for factor in range(2, self.gene_number+1):
             factorial *= factor
         self.total_perm = factorial
     
-    def generate_stock(self) -> List[int]:
+    def generate_stock(self) -> None:
+        """
+        Generates the stock of available numbers for permutations.
+        """
         self.stock = list(range(1, self.gene_number+1)) * self.total_perm
     
-    def generate_permutations(self):
+    def generate_permutations(self) -> None:
+        """
+        Generates unique permutations using the available stock.
+        """
         permutation_list = []
         while len(self.stock) != 0:
             current_permutation = []
@@ -46,24 +70,24 @@ class PermutationList:
         
         self.permutations = permutation_list
     
-    def output(self):
+    def output(self) -> None:
+        """
+        Outputs the total number of permutations and the generated permutations.
+        """
         print(self.total_perm)
         for permutation in self.permutations:
             print(" ".join(map(str, permutation)))
-                
     
-            
-            
-        
-            
-   
-            
+def main():
+    """
+    The main function to execute the permutation generation and output.
+    """
+    permutation_list_instance = PermutationList()
+    permutation_list_instance.input()
+    permutation_list_instance.total_permutations()
+    permutation_list_instance.generate_stock()
+    permutation_list_instance.generate_permutations()
+    permutation_list_instance.output()
 
-"""  
-tester = PermutationList()
-tester.input()
-tester.total_permutations()
-tester.generate_stock()
-tester.generate_permutations()
-tester.output()
-"""
+if __name__ == "__main__":
+    main()
