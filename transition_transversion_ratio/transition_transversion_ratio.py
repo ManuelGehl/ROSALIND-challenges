04d585ed984c224e56234c8515efb1c6ab6ee67f
Manuel Gehl <127943149+ManuelGehl@users.noreply.github.com>
2024-02-28 17:39:12 +0100

    Working raw version

--- transition_transversion_ratio/transition_transversion_ratio.py
+++ transition_transversion_ratio/transition_transversion_ratio.py
@@ -39,23 +39,6 @@ class TransitionTransversionMeasurer:
         # Output sequences
         self.sequence1, self.sequence2 = seq_dict.values()
         
-    def base_mapping(self, sequence: str) -> str:
-
-    
-        # Ensure that sequence is in upper case
-        sequence = sequence.upper()
-        # Loop trough the sequence and map each base to the appropriate category
-        purine_pyrimidine_seq = []
-        for base_index in range(0, len(sequence)):
-            current_base = PURINE_PYRIMIDINE_MAPPING.get(sequence[base_index], None)
-            # Print a message when unknown character is in sequence
-            if current_base is None:
-                print(f"Unknown base at position: {base_index}")
-            else:
-                purine_pyrimidine_seq.append(current_base)
-        # Combine list to string
-        return "".join(purine_pyrimidine_seq)
-    
     def count_transitions_transversions(self, sequence1, sequence2) -> None:
         # Initialize counters
         transition_counter = 0
@@ -65,10 +48,13 @@ class TransitionTransversionMeasurer:
             raise ValueError("Sequences must be of equal length")
         
         for i in range(len(sequence1)):
-            if sequence1[i] == sequence2[i]:
-                transition_counter += 1
-            else:
-                transversion_counter += 1
+            # First check if bases are equal
+            if sequence1[i] != sequence2[i]:
+                # Then check if transition or transversion
+                if PURINE_PYRIMIDINE_MAPPING[sequence1[i]] == PURINE_PYRIMIDINE_MAPPING[sequence2[i]]:
+                    transition_counter += 1
+                else:
+                    transversion_counter += 1
         # Output results
         self.transition_counter = transition_counter
         self.transversion_counter = transversion_counter
@@ -84,9 +70,7 @@ def main():
     measurer = TransitionTransversionMeasurer()
     measurer.read_sequences()
     seq1, seq2 = measurer.sequence1, measurer.sequence2
-    seq1_transformed = measurer.base_mapping(sequence=seq1)
-    seq2_transformed = measurer.base_mapping(sequence=seq2)
-    measurer.count_transitions_transversions(seq1_transformed, seq2_transformed)
+    measurer.count_transitions_transversions(seq1, seq2)
     measurer.calculate_ratio()
 
 if __name__ == "__main__":
