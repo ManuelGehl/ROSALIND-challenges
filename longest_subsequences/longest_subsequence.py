8b505c68521139f569b214f9c6a3dcc056427fa2
Manuel Gehl <127943149+ManuelGehl@users.noreply.github.com>
2024-03-31 12:42:38 +0200

    Completed decreasing subsequence function

--- longest_subsequences/longest_subsequence.py
+++ longest_subsequences/longest_subsequence.py
@@ -2,8 +2,8 @@ class LongestSubsequences:
     
     def __init__(self):
         self.permutation = None
-        self.increasing_seq = list()
-        self.decreasing_seq = list()
+        self.increasing_seq = None
+        self.decreasing_seq = None
 
     def remove_maximum(self, window: list) -> list:
         window.remove(max(window))
@@ -16,20 +16,48 @@ class LongestSubsequences:
                 return False
         
         return True
+    
+    def decreasing_series(self, window: list) -> bool:
+        for step in range(len(window) - 1):
+            number1, number2 = window[step: step + 2]
+            if number1 < number2:
+                return False
+        
+        return True
       
     def remove_minimum(self, window: list) -> list:
         window.remove(min(window))
         return window
 
-        
+        # 1, 2, 3
     def find_increasing_subseq(self):
         # Initialize current window and longest sequence
-        current_window = self.permutation
+        current_window = self.permutation.copy()
         
         while self.increasing_series(current_window) == False:
             current_window = self.remove_maximum(current_window)
         
-        self.increasing_seq = current_window        
+        self.increasing_seq = current_window
+    
+    def find_decreasing_subseq(self):
+        # Initialize current window and longest sequence
+        current_window = self.permutation.copy()
+        
+        while self.decreasing_series(current_window) == False:
+            current_window = self.remove_minimum(current_window)
+        
+        self.decreasing_seq = current_window
+
+def main():
+    subsequence_finder = LongestSubsequences()
+    subsequence_finder.permutation = [5, 1, 4, 2, 3]
+    subsequence_finder.find_increasing_subseq()
+    subsequence_finder.find_decreasing_subseq()
+    print(subsequence_finder.increasing_seq)
+    print(subsequence_finder.decreasing_seq)
+
+if __name__ == "__main__":
+    main()
            
 
         
