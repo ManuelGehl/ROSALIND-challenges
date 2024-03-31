fa73e36e8abf37a78318cb07f1cfbd2647d1bd56
Manuel Gehl <127943149+ManuelGehl@users.noreply.github.com>
2024-03-31 12:28:34 +0200

    Changed algorithm to top-down

--- longest_subsequences/longest_subsequence.py
+++ longest_subsequences/longest_subsequence.py
@@ -8,54 +8,30 @@ class LongestSubsequences:
     def remove_maximum(self, window: list) -> list:
         window.remove(max(window))
         return window
-    
-    def check_remaining_window(self, window: list, sequence: list) -> bool:
-        if len(window) < len(sequence):
-            return True
-        else:
-            return False
+
+    def increasing_series(self, window: list) -> bool:
+        for step in range(len(window) - 1):
+            number1, number2 = window[step: step + 2]
+            if number1 > number2:
+                return False
+        
+        return True
+      
+    def remove_minimum(self, window: list) -> list:
+        window.remove(min(window))
+        return window
+
         
     def find_increasing_subseq(self):
         # Initialize current window and longest sequence
         current_window = self.permutation
-        longest_seq = []
         
-        # Iterate as often as numbers are in the permutation sequence
-        for _ in range(len(self.permutation)):
-            
-            # Check if longest_seq is longer than all sequences before
-            if len(longest_seq) >= len(self.increasing_seq):
-                self.increasing_seq = longest_seq
+        while self.increasing_series(current_window) == False:
+            current_window = self.remove_maximum(current_window)
+        
+        self.increasing_seq = current_window        
+           
 
-            # Loop trough window
-            longest_seq = [current_window[0]]
-            for next_number in current_window[1:]:
-                
-                # Determine highest number in window
-                highest_number = max(current_window)
-                
-                # Check if number at position 0 is maximum
-                if current_window[0] == highest_number:
-                    # Remove maximum value and exit loop
-                    current_window = self.remove_maximum(window=current_window)
-                    break
-                
-                # Check if next number is greater than numbers in potential sequence
-                if longest_seq[-1] < next_number:
-                    longest_seq.append(next_number)
-    
-                # If next number is already maximum of current window, remove maximum value and exit loop
-                if next_number == highest_number:
-                    current_window = self.remove_maximum(window=current_window)
-                    break
-        
-            
-            print(longest_seq)
-            # Define current window new by removing the maximum value from array
-            
-            # Check if the remainind window size can produce a sequence that is longer than the current longest sequence
-            #if self.check_remaining_window(window=current_window, sequence=self.increasing_seq):
-            #    break
         
             
             
